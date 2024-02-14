31942114e3d6d8749b4deb04cbcc0ca4cbc8e6c9
mjthurber <mjthurber@gmail.com>
2024-02-14 14:25:26 -0700

    reapplied About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,30 +1,24 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import About from './pages/About.jsx';
-import Contact from './pages/Contact.jsx';
-import Portfolio from './pages/Portfolio.jsx';
-import Resume from './pages/Resume.jsx';
-
-import './index.css'
-
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { path: 'About', element: <About /> },
-      { path: 'Contact', element: <Contact /> },
-      { path: 'Portfolio', element: <Portfolio /> },
-      { path: 'Resume', element: <Resume /> }
-    ]
+function About() {
+    return (
+  <div className="about-me-container">
+        <section className="about-me-content">
+          <h2>About Me</h2>
+          <p>
+            Hello! I'm Matthew Thurber, a web developer with a background in finance. I am passionate about creating
+            engaging and user-friendly web applications. My journey into web development started with a curiosity
+            to combine my analytical skills from finance with the boundless creativity of web development.
+          </p>
+          <p>
+            In my projects, I strive to deliver solutions that not only meet technical requirements but also provide
+            a positive and seamless user experience. I enjoy learning new technologies and staying up-to-date with the
+            latest trends in web development.
+          </p>
+          <p>
+            When I'm not coding, you can find me exploring new technologies, travelling and camping, or playing Fortnite with my wife.
+          </p>
+        </section>
+      </div>
+    );
   }
-]);
-
-
-ReactDOM
-.createRoot(document.getElementById('root'))
-.render(<RouterProvider router={router} />
-)
+  
+  export default About;
