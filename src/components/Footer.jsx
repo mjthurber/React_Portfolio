bd9aed19251c0ca4fe3019e55e9706ca121e0c0e
mjthurber <mjthurber@gmail.com>
2024-02-14 22:28:46 -0700

    capitalization is the worst

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import gitHubCat from '../assets/GithubCat.png';
-import linkedIn from '../assets/linkedinIcon.png';
+import linkedIn from '../assets/LinkedinIcon.png';
 import './Footer.css';
 
 function Footer() {
